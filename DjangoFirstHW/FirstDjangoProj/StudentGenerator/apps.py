4fc442ab6fb096e534d0cbe31db01a580a45718e
Eugene Mytrik <104303531+EugenMytrik@users.noreply.github.com>
2024-01-04 12:30:20 +0200

    Add files via upload

--- DjangoFirstHW/FirstDjangoProj/StudentGenerator/apps.py
+++ DjangoFirstHW/FirstDjangoProj/StudentGenerator/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class StudentgeneratorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'StudentGenerator'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "StudentGenerator"
